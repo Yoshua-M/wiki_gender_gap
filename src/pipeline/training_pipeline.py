9a9aab80a6519d1a0519c842d89abd08e31705e7
Yoshua-M <yoshua.marquez.g@gmail.com>
2024-05-02 12:24:34 -0600

    Requirements added to the project.

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,7 +1,6 @@
 """
 Training and assesing a model
 """
-
 import pandas as pd
 from src.pipeline import data_pipeline
 from sklearn.linear_model import LogisticRegression
@@ -9,9 +8,11 @@ from sklearn.linear_model import LogisticRegression
 from src.utils import Kernel
 from src.components import model_trainer
 
-kernel = Kernel()
-df = pd.read_csv('src/mlops_data.csv')
-df = data_pipeline.run(df, kernel)
+if __name__ == '__main__':
 
-model = LogisticRegression(max_iter=1000)
-model_trainer.train_and_evaluate(model, df, kernel)
+    kernel = Kernel()
+    df = pd.read_csv(kernel.config['path_to_data'])
+    df = data_pipeline.run(df, kernel)
+
+    model = LogisticRegression(max_iter=1000)
+    model_trainer.train_and_evaluate(model, df, kernel)
